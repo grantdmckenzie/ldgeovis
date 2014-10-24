6302d7bbae1afc0b07833db48b9e375767875af1
Grant McKenzie <grantdmckenzie@gmail.com>
2014-10-24 12:06:09 -0700

    minor ui updates

--- js/map.js
+++ js/map.js
@@ -12,7 +12,7 @@
       L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 	      maxZoom: 18,
 	     //  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ',
-	      id: 'grantdmckenzie.i22p4k2n'
+	      id: 'grantdmckenzie.i22pkp10'
       }).addTo(_map); 
     
       
